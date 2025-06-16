cbfac58a74d73fe1c142b70815e4eb71401a0677
Junthh <lucnm007@gmail.com>
2025-06-16 16:04:51 +0700

    done admin

---
+++ Admin/app/models/product.js
@@ -0,0 +1,16 @@
+class Product {
+  constructor(_id, _name, _price,  _img, _screen, _backCamera, _fontCamera, _desc, _type){
+    this.id = _id;
+    this.name = _name;
+    this.price = _price;
+    this.img = _img;
+    this.screen = _screen;
+    this.backCamera = _backCamera;
+    this.fontCamera = _fontCamera;
+    this.desc = _desc;
+    this.type = _type;
+  }
+
+}
+
+export default Product;
